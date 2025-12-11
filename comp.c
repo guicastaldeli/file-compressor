f46919fa2ce04530bfd0f87dbdd1199517c29360
gui castaldeli <guicastaldeli8@gmail.com>
2025-12-10 22:58:22 -0400

    update-01_08

--- comp.c
+++ comp.c
@@ -28,4 +28,19 @@ CompressionType detectBestCompression(const uint8_t* data, size_t size) {
             maxFreq = byteFreq[data[i]];
         }
     }
+
+    int textBytes = 0;
+    for(int i = 32; i < 127; i++) textBytes += byteFreq[i];
+    textBytes += 
+        byteFreq['\t'] + 
+        byteFreq['\n'] + 
+        byteFreq['\r'];
+    if(textBytes * 100 / size > 70) {
+        return COMP_BP;
+    }
+
+    int runCount = 0;
+    for(size_t i = 1; i < size; i++) {
+        if(data[i] == data[i-1]) runCount++;
+    }
 }
